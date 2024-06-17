5c8e6e24b5f96e66023a17e4ebec340ce1fc520b
Abhinavv69 <abhinavv2005@gmail.com>
2024-06-17 22:46:54 +0530

    search-in-Linked-List question added

--- Linked List/1D LL/searchInLL.java
+++ Linked List/1D LL/searchInLL.java
@@ -1,3 +1,26 @@
-public class searchInLL {
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class searchInLL {
+    public boolean searchKey(Node head, int key) {
+        Node current = head; // Start from the head node
+
+        // Traverse the list until we reach the end
+        while (current != null) {
+            if (current.data == key) {
+                return true; // Key is found
+            }
+            current = current.next; // Move to the next node
+        }
+
+        return false; // Key is not found in the list
+    }
 
 }
