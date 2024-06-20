221d4dc03d68724fc3a80749f4f6cec7b5fcd0df
Abhinavv69 <abhinavv2005@gmail.com>
2024-06-20 10:44:45 +0530

    middle_of_Linked_List question added

---
+++ Linked List/Medium-Level.java/middle_of_LL.java
@@ -0,0 +1,33 @@
+/*
+ * Given the head of a singly linked list, return the middle ListNode of the linked
+ * list.
+ * 
+ * If there are two middle ListNodes, return the second middle ListNode.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
