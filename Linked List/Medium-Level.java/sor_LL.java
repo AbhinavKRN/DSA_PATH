5fa463cb0728327b87eeca91ae11f2ee9f82ab1c
Abhinavv69 <abhinavv2005@gmail.com>
2024-06-23 19:28:16 +0530

    Sort_-LinkedList question added

---
+++ Linked List/Medium-Level.java/sor_LL.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode mid = mid(head);
+        ListNode newhead = mid.next;
+        mid.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(newhead);
+        return merge(left, right);
+    }
+
+    public ListNode mid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode neww = new ListNode(-1);
+        ListNode temp = neww;
+        while (head1 != null && head2 != null) {
+            if (head1.val < head2.val) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+        if (head1 != null) {
+            temp.next = head1;
+        }
+        if (head2 != null) {
+            temp.next = head2;
+        }
+        return neww.next;
+    }
+}
